3632d99a31c550297e36bbc073bbfdd2ad47697e
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-01-23 14:01:02 +0800

    add : lock save counters

---
+++ consumer/src/state/instance.py
@@ -0,0 +1,5 @@
+from .models import CounterModel , TotalQueryCountModel , AvgExecutionTimeModel
+
+Counter = CounterModel(0)
+TotalQueryCount = TotalQueryCountModel(0)
+AvgExecutionTime = AvgExecutionTimeModel(0.0)
