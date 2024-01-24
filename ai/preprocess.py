46bf51a9428a0ea0cfb4916aee6657b71a1587d4
Jerry Lee <oojerryleeoo@gmail.com>
2024-01-25 02:02:02 +0800

    refactor: Update preprocess function

--- ai/preprocess.py
+++ ai/preprocess.py
@@ -3,19 +3,15 @@ import os
 import pandas as pd
 
 
-def preprocess_official_log_data(data_dir: str) -> pd.DataFrame:
-    log_df = pd.DataFrame()
+def preprocess_metric_data(data_dir: str) -> pd.DataFrame:
+    metric_df = pd.DataFrame()
     for filename in os.listdir(data_dir):
-        # Skip the container startup latency file
-        if filename == "Container Startup Latency.csv":
-            continue
-
-        if log_df.empty:
-            log_df = pd.read_csv(os.path.join(data_dir, filename))
+        if metric_df.empty:
+            metric_df = pd.read_csv(os.path.join(data_dir, filename))
         else:
             tmp_df = pd.read_csv(os.path.join(data_dir, filename))
 
-            # Merge log_df and tmp_df along the "Time" column
-            log_df = pd.merge(log_df, tmp_df, on="Time")
+            # Merge metric_df and tmp_df along the "Time" column
+            metric_df = pd.merge(metric_df, tmp_df, on="Time", how="outer")
 
-    return log_df
+    return metric_df
