3cc8c961527c87696190f9df51391c0fa0140663
peterxcli <peterxcli@gmail.com>
2024-01-26 14:17:09 +0800

    refactor: refactor log parsing and merging code

--- monitor_runner.py
+++ monitor_runner.py
@@ -1,12 +1,8 @@
 import asyncio
 import re
-import threading
-import time
 
 import pandas as pd
 
-from ai.analyze import analyze_by_llm
-from bot.bot import broadcast, client, run_bot
 from monitor.service import cloudrun, log
 
 cloudrun.CloudRunManager()
@@ -55,20 +51,73 @@ def fill_missing_columns(metric_df) -> pd.DataFrame:
 
 async def main():
     print("Running monitor runner...")
-    logDF: pd.DataFrame = pd.DataFrame()
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
+
+    # bot_thread = threading.Thread(target=run_bot)
+    # bot_thread.start()
+    cloudrun_manager = cloudrun.CloudRunManager()
+
     while True:
-        for log_line in log.tail_log_entry(service_name="consumer-sentry"):
+        print("fetching...")
+        log_df: pd.DataFrame = pd.DataFrame()
+        for log_line in log.tail_log_entry(
+            service_name="consumer-sentry", max_results=10
+        ):
             parse_df = parser(log_line)
-            logDF = pd.concat([logDF, parse_df], ignore_index=True)
-            with open("log_data.csv", "w") as f:
-                logDF.to_csv(f, index=False)
-            result = analyze_by_llm(fill_missing_columns(logDF))
-            asyncio.run_coroutine_threadsafe(
-                broadcast(message_dict=result), client.loop
-            )
-        time.sleep(10)
+            log_df = pd.concat([log_df, parse_df], ignore_index=True)
+
+        log_df["Time"] = log_df["time"]
+        log_df = log_df.drop(columns=["time"])
+        # convert `Time` column to datetime format eg. 2024-01-26 08:51:26
+        log_df["Time"] = pd.to_datetime(log_df["Time"], format="%Y-%m-%d %H:%M:%S")
+        # log_df = log_df.set_index("Time")
+
+        # print(log_df)
+
+        # get the min and max time
+        min_time = int(log_df["Time"].min().timestamp())
+        max_time = int(log_df["Time"].max().timestamp())
+        # print(min_time, max_time, type(min_time), type(max_time))
+        # TODO: convert the min and max time to the time.time() format
+
+        metrics_df: pd.DataFrame = cloudrun_manager.get_metrics(
+            "consumer-sentry", min_time, max_time
+        )
+        # print(metrics_df)
+        # metrics_df["Time"] = pd.to_datetime(metrics_df["Time"])
+        # log_df["Time"] = pd.to_datetime(log_df["Time"])
+
+        # log_df.to_csv("log.csv")
+        # metrics_df.to_csv("metrics.csv")
+        # print(metrics_df.columns, log_df.columns)
+        metrics_df["Time"] = pd.to_datetime(metrics_df["Time"])
+        log_df["Time"] = pd.to_datetime(log_df["Time"]).dt.floor("s")
+
+        # Set a 60 seconds tolerance for merging
+        tolerance = pd.Timedelta(seconds=60)
+
+        # Perform an asof merge with a tolerance of 60 seconds
+        # merged_df = pd.merge_asof(
+        #     metrics_df.sort_values("Time"),
+        #     log_df.sort_values("Time"),
+        #     on="Time",
+        #     tolerance=tolerance,
+        #     direction="nearest",
+        # )
+        merged_df = pd.merge(log_df, metrics_df, on="Time", how="outer")
+
+        with open("merge.csv", "a") as f:
+            merged_df.to_csv(f, mode="a", header=f.tell() == 0, index=False)
+        # result = analyze_by_llm(fill_missing_columns(merged_df))
+
+        # pd.
+        # result = analyze_by_llm(fill_missing_columns(merge_df))
+        # asyncio.run_coroutine_threadsafe(
+        #     broadcast(message_dict=result), client.loop
+        # )
+
+        # append to the csv file
+        # with open("merge_data.csv", "a") as f:
+        #     merge_df.to_csv(f, mode="a", header=f.tell() == 0)
 
 
 asyncio.run(main())
